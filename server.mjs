d9e9e11c6c55ce92994e33cca692175486e5940a
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-10-14 19:49:15 -0400

    setting up files, app, etc.

---
+++ server.mjs
@@ -0,0 +1,8 @@
+// 1. create server.mjs file
+// 2. npm init -y - creates package.json
+// 3. correct package.json
+// 4. npm i express - download express
+// 5. import express at top of page
+// 6. Initialize express into a variable
+// 7. Listen to express(app) at the BOTTOM of the page
+
