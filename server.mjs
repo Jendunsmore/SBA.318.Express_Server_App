3b5849bdb3ba80c2443a88936be987ed9faeee49
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-10-15 01:18:47 -0400

    edited and added to server.mjs

--- server.mjs
+++ server.mjs
@@ -26,7 +26,7 @@ const logRequest = (req, res, next) => {
     next();
 };
 
-// Custom middleware to simulate a page when in maintenance mode (?)
+// Custom middleware for maintenance mode
 const maintenanceMode = (req, res, next) => {
     const isInMaintenance = false; // set to true to enable maintenance mode
     if (isInMaintenance) {
@@ -40,10 +40,57 @@ const maintenanceMode = (req, res, next) => {
 app.use(logRequest);
 app.use(maintenanceMode);
 
+// view engine
+app.engine('file', (filepath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) return callback(err);
+
+        // Check if rendering all soctumes, trivia, or scores
+        if (options.allCostumes) {
+            let results = '';
+            options.allCostumes.forEach(el => {
+                result += `<h2>Costume: ${el.name}</h2><h3>Type: ${el.type}</h3><br>`;
+            });
+            const rendered = content.toString().replace('#content#', result);
+            return callback(null, rendered);
+
+        } else if (options.allTrivia) {
+            let result = '';
+            options.allTrivia.forEach(el => {
+                result += `<h2>Question: ${el.question}</h2><h3>Answer: ${el.answer}</h3><br>`;
+            });
+            const rendered = content.toString().replace('#content#', result);
+            return callback(null, rendered);
+
+        } else if (options.allScores) {
+            let result = '';
+            options.allScores.forEach(el => {
+                result += `<h2>Player: ${el.playerName}</h2><h3>Score: ${el.score}</h3><br>`;
+            });
+            const rendered = content.toString().replace('#content#', result);
+            return callback(null, rendered);
+        }
+
+        // Fallback for single item views
+        const rendered = content
+            .toString()
+            .replaceAll('#name#', `${options.name || 'N/A'}`)
+            .replace('#type#', `${options.type || 'N/A'}`)
+            .replace('#question#', `${options.question || 'N/A'}`)
+            .replace('#answer#', `${options.answer || 'N/A'}`)
+            .replace('#playerName#', `${options.playerName || 'N/A'}`)
+            .replace('#score#', `${options.score || 'N/A'}`);
+
+        return callback(null, rendered);
+    });
+});
+
+
+
 
 // Body parser- form submission
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.use(bodyParser.json());
 
 // template engine
 app.set('view engine', 'file');
