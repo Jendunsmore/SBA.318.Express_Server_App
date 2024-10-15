e2890c8817d369c439cec5ba73a56842b9f8f3ec
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-10-14 20:27:56 -0400

    working within server.mjs, setting up

--- server.mjs
+++ server.mjs
@@ -6,3 +6,15 @@
 // 6. Initialize express into a variable
 // 7. Listen to express(app) at the BOTTOM of the page
 
+// Imports
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+
+
+// Initialize Express
+const app = express();
+let PORT = 3000;
+
+// Middleware to serve static files
+app.use(express.static('./styles'));
